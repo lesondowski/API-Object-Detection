b384601c8a796181573cc71056149ca69390cd81
lesondowski <lsonofficial09@gmail.com>
2025-11-25 15:41:05 +0700

    Upadte results

--- utils_cf/config.py
+++ utils_cf/config.py
@@ -2,5 +2,5 @@ import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-MODEL_OD_PT = "api/files/AD_YL11s_1.0.pt"
+MODEL_OD_PT = "api/files/ADYL11m.pt"
 MODEL_IS_ONNX = "api/files/AD_YL11s_Seg_1.80.onnx"
