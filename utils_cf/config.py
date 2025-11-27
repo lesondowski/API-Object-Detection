fb6cf76ba51b660e2d981af04e9a7f4434b4409a
lesondowski <lsonofficial09@gmail.com>
2025-11-27 14:04:15 +0700

    update path config

--- utils_cf/config.py
+++ utils_cf/config.py
@@ -2,7 +2,7 @@ import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-MODEL_OD_PT = "api/files/ADYL11m.pt"
-MODEL_IS_ONNX = "api/files/AD_YL11s_Seg_1.80.onnx"
+MODEL_OD_PT = "api/files/model/pt/ADYL11m.pt"
+MODEL_IS_ONNX = "api/files/model/onnx/AD_YL11s_Seg_1.80.onnx"
 
-MODEL_BLOCK_PT = "api/files/AD_Productblock_YL11.pt"
+MODEL_BLOCK_PT = "api/files/model/pt/AD_Productblock_YL11.pt"
